c26fa35d70c0491c39db5de01d8cbffabdaecfbd
Djobbo "Alfie" Maïga <djobbo.maiga@gmail.com>
2020-06-01 20:00:22 +0200

    player stats overview file split

---
+++ src/client/pages/stats/PlayerPage/SectionOverview/BestLegendRankedStats/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+interface Props {
+    legend: string;
+    rating: number;
+    peak: number;
+    games: number;
+    wins: number;
+    losses: number;
+    winrate: string;
+}
+
+export const BestLegendRankedStats: React.FC<Props> = ({
+    legend = 'Random',
+    rating = 750,
+    peak = 750,
+    games = 0,
+    wins = 0,
+    losses = 0,
+    winrate = '0',
+}: Props) => {
+    return (
+        <div className="ranked-stats">
+            <p>
+                <span className="stat stat-large">{legend}</span>
+                <span className="stat-desc"> Best Legend</span>
+            </p>
+            <p>
+                <span className="stat stat-medium">{rating}</span>
+                <span className="stat-desc"> Elo</span>
+            </p>
+            <p>
+                <span className="stat">{peak}</span>
+                <span className="stat-desc"> Peak</span>
+            </p>
+            <hr />
+            <p>
+                <span className="stat">{games}</span>
+                <span className="stat-desc"> Games </span>
+                <span className="stat">
+                    ({wins}W-{losses}L)
+                </span>
+            </p>
+            <p>
+                <span className="stat">{winrate}%</span>
+                <span className="stat-desc"> Winrate</span>
+            </p>
+            <hr />
+            <img src={`/assets/images/icons/legends/${legend}.png`} alt={`${legend}`} className="legend-icon" />
+        </div>
+    );
+};
--- src/client/pages/stats/PlayerPage/SectionOverview/index.tsx
+++ src/client/pages/stats/PlayerPage/SectionOverview/index.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import './styles.scss';
 import { IPlayerSeasonFormat, ILegendStatsFormat } from 'corehalla.js';
 
+import { RankedStats } from './RankedStats';
+import { BestLegendRankedStats } from './BestLegendRankedStats';
+
 interface Props {
     season: IPlayerSeasonFormat;
     bestLegend: ILegendStatsFormat;
@@ -39,92 +42,5 @@ export const SectionOverview: React.FC<Props> = ({ season, bestLegend }: Props) 
                 </div>
             </div>
         </section>
-    );
-};
-
-const RankedStats = ({
-    tier = 'Tin 0',
-    rating = 750,
-    peak = 750,
-    games = 0,
-    wins = 0,
-    losses = 0,
-    winrate = '0',
-    region = 'N/A',
-}) => {
-    return (
-        <div className="ranked-stats">
-            <p>
-                <span className="stat-desc">Tier </span>
-                <span className="stat stat-large">{tier}</span>
-            </p>
-            <p>
-                <span className="stat-desc">Elo </span>
-                <span className="stat stat-medium">{rating}</span>
-            </p>
-            <p>
-                <span className="stat-desc">Peak </span>
-                <span className="stat">{peak}</span>
-            </p>
-            <hr />
-            <p>
-                <span className="stat">{games}</span>
-                <span className="stat-desc"> Games </span>
-                <span className="stat">
-                    ({wins}W-{losses}L)
-                </span>
-            </p>
-            <p>
-                <span className="stat-desc">Winrate </span>
-                <span className="stat">{winrate}%</span>
-            </p>
-            <hr />
-            <img
-                src={`/assets/images/icons/flags/${region.toUpperCase()}.png`}
-                alt={`${region}_flag`}
-                className="region-icon"
-            />
-        </div>
-    );
-};
-
-const BestLegendRankedStats = ({
-    legend = 'Random',
-    rating = 750,
-    peak = 750,
-    games = 0,
-    wins = 0,
-    losses = 0,
-    winrate = '0',
-}) => {
-    return (
-        <div className="ranked-stats">
-            <p>
-                <span className="stat stat-large">{legend}</span>
-                <span className="stat-desc"> Best Legend</span>
-            </p>
-            <p>
-                <span className="stat stat-medium">{rating}</span>
-                <span className="stat-desc"> Elo</span>
-            </p>
-            <p>
-                <span className="stat">{peak}</span>
-                <span className="stat-desc"> Peak</span>
-            </p>
-            <hr />
-            <p>
-                <span className="stat">{games}</span>
-                <span className="stat-desc"> Games </span>
-                <span className="stat">
-                    ({wins}W-{losses}L)
-                </span>
-            </p>
-            <p>
-                <span className="stat">{winrate}%</span>
-                <span className="stat-desc"> Winrate</span>
-            </p>
-            <hr />
-            <img src={`/assets/images/icons/legends/${legend}.png`} alt={`${legend}`} className="legend-icon" />
-        </div>
     );
 };
