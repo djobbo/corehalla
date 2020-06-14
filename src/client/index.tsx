b0e2167590c6177c1677144c00503b87329f052d
Djobbo "Alfie" Maïga <djobbo.maiga@gmail.com>
2020-06-14 21:37:44 +0200

    deleted serviceworker II

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
-
-serviceWorker.unregister();
