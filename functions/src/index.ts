945bfc3e0a04c9429c81ae2d04a661a0d4c7c3a3
Djobbo "Alfie" Maïga <djobbo.maiga@gmail.com>
2020-06-15 17:40:17 +0200

    clan stats function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,8 +13,8 @@ const app = express();
 app.use(cors({ origin: true }));
 
 const cacheTime = {
-	client: '120',
-	server: '180',
+    client: '120',
+    server: '180',
 };
 
 const apiCacheControl = `public, max-age=${cacheTime.client}, s-maxage=${cacheTime.server}`;
@@ -22,57 +22,69 @@ const apiCacheControl = `public, max-age=${cacheTime.client}, s-maxage=${cacheTi
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.status(200).json('Corehalla API');
+    res.status(200).json('Corehalla API');
 });
 
 router.get('/rankings/:bracket?/:region?/:page?', (req, res) => {
-	const bracket = req.params.bracket || '1v1';
-	const region = (req.params.region || 'ALL') as RankedRegion;
-	const page = req.params.page || 1;
+    const bracket = req.params.bracket || '1v1';
+    const region = (req.params.region || 'ALL') as RankedRegion;
+    const page = req.params.page || 1;
 
-	const name = (req.query.player as string) || (req.query.p as string) || '';
+    const name = (req.query.player as string) || (req.query.p as string) || '';
 
-	res.set('Cache-Control', apiCacheControl);
+    res.set('Cache-Control', apiCacheControl);
 
-	if (bracket === '2v2') {
-		bhAPI
-			.fetch2v2RankingsFormat({ region, page, name })
-			.then((data) => {
-				res.status(200).json(data);
-			})
-			.catch((e) => {
-				res.status(500).json('Failed to fetch 1v1 Rankings!');
-			});
-	} else if (bracket === '1v1') {
-		bhAPI
-			.fetch1v1RankingsFormat({ region, page, name })
-			.then((data) => {
-				res.status(200).json(data);
-			})
-			.catch((e) => {
-				res.status(500).json('Failed to fetch 2v2 Rankings!');
-			});
-	} else {
-		res.status(400).json('Bad Request');
-	}
+    if (bracket === '2v2') {
+        bhAPI
+            .fetch2v2RankingsFormat({ region, page, name })
+            .then((data) => {
+                res.status(200).json(data);
+            })
+            .catch((e) => {
+                res.status(500).json('Failed to fetch 1v1 Rankings!');
+            });
+    } else if (bracket === '1v1') {
+        bhAPI
+            .fetch1v1RankingsFormat({ region, page, name })
+            .then((data) => {
+                res.status(200).json(data);
+            })
+            .catch((e) => {
+                res.status(500).json('Failed to fetch 2v2 Rankings!');
+            });
+    } else {
+        res.status(400).json('Bad Request');
+    }
 });
 
 router.get('/stats/player/:id', (req, res) => {
-	const id = parseInt(req.params.id);
-	res.set('Cache-Control', apiCacheControl);
-	bhAPI
-		.fetchPlayerFormat(id)
-		.then((data) => {
-			res.status(200).json(data);
-		})
-		.catch((e) => {
-			console.error(e);
-			res.status(500).json('Internal server error');
-		});
+    const id = parseInt(req.params.id);
+    res.set('Cache-Control', apiCacheControl);
+    bhAPI
+        .fetchPlayerFormat(id)
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((e) => {
+            console.error(e);
+            res.status(500).json('Internal server error');
+        });
+});
+
+router.get('/stats/clan/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    res.set('Cache-Control', apiCacheControl);
+    bhAPI
+        .fetchClanFormat(id)
+        .then((data) => res.status(200).json(data))
+        .catch((e) => {
+            console.error(e);
+            res.status(500).json('Internal server error');
+        });
 });
 
 router.get('/**', (req, res) => {
-	res.status(404).json('Not Found!');
+    res.status(404).json('Not Found!');
 });
 
 app.use('/api', router);
