51575dbf21fa0a2a0af11fe3c22ae531dd098245
Djobbo "Alfie" Maïga <djobbo.maiga@gmail.com>
2020-06-23 00:07:30 +0200

    sidebar setup

--- src/client/components/TopBar/Sidebar/index.tsx
+++ src/client/components/TopBar/Sidebar/index.tsx
@@ -1,7 +1,24 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+
+import { FavoritesContext } from '../../../FavoritesProvider';
 
 import './styles.scss';
+import { Link } from 'react-router-dom';
+import { Searchbar } from '../Searchbar';
 
 export const Sidebar: FC = () => {
-    return <div className="sidebar"></div>;
+    const { favorites } = useContext(FavoritesContext);
+    return (
+        <div className="sidebar">
+            <Searchbar />
+            <div>
+                <h2>Favorites</h2>
+                {favorites.players.map((fav) => (
+                    <div key={fav.id}>
+                        <Link to={fav.link}>{fav.name}</Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
