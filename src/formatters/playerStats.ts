a7d0841cb828c0074d0702d3c4dd25fda2832ce0
Victor Monsallier <corehallabusiness@gmail.com>
2020-09-21 11:24:15 +0000

    local 2v2 fix Fixes #4, Fixes #5

--- src/formatters/playerStats.ts
+++ src/formatters/playerStats.ts
@@ -373,32 +373,40 @@ export function formatTeamsStats(
     playerID: number,
     teamsStats: I2v2Team[]
 ): I2v2TeamFormat[] {
-    return teamsStats.map<I2v2TeamFormat>(
-        ({
-            brawlhalla_id_one,
-            brawlhalla_id_two,
-            teamname,
-            region,
-            global_rank: _tgr,
-            peak_rating,
-            ...season
-        }) => {
-            const isPlayerOne = brawlhalla_id_one === playerID;
-            const playerNames = teamname.split('+');
+    return teamsStats
+        .map<I2v2TeamFormat>(
+            ({
+                brawlhalla_id_one,
+                brawlhalla_id_two,
+                teamname,
+                region,
+                global_rank: _tgr,
+                peak_rating,
+                ...season
+            }) => {
+                const isPlayerOne = brawlhalla_id_one === playerID;
+                const playerNames = teamname.split('+');
 
-            return {
-                playerAlias: cleanString(playerNames[+!isPlayerOne]),
-                teammate: {
-                    id: isPlayerOne ? brawlhalla_id_two : brawlhalla_id_one,
-                    name: cleanString(playerNames[+isPlayerOne]),
-                },
-                region: regions[region - 2],
-                season: {
-                    ...season,
-                    peak: peak_rating,
-                    ratingSquash: getHeroRatingSquash(season.rating),
-                },
-            };
-        }
-    );
+                return {
+                    playerAlias: cleanString(playerNames[+!isPlayerOne]),
+                    teammate: {
+                        id: isPlayerOne ? brawlhalla_id_two : brawlhalla_id_one,
+                        name: cleanString(playerNames[+isPlayerOne]),
+                    },
+                    region: regions[region - 2],
+                    season: {
+                        ...season,
+                        peak: peak_rating,
+                        ratingSquash: getHeroRatingSquash(season.rating),
+                    },
+                };
+            }
+        )
+        .reduce<I2v2TeamFormat[]>(
+            (acc, x) =>
+                acc.find((y) => y.teammate.id === x.teammate.id)
+                    ? acc
+                    : [...acc, x],
+            []
+        );
 }
