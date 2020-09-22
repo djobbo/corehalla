3eeaf62c264110ef976b0261af2d5c6de533c8f4
Victor Monsallier <corehallabusiness@gmail.com>
2020-09-22 10:40:23 +0000

    rankings1v1Format tests

--- src/formatters/index.ts
+++ src/formatters/index.ts
@@ -1,5 +1,6 @@
 export * from './playerStats';
 export * from './clan';
-export * from './rankings';
+export * from './rankings1v1';
+export * from './rankings2v2';
 export * from './gloryCalculator';
 export * from './powerRankings';
