4305b9983db535c20e5e8f8be277916269d24413
Djobbo Maïga <corehallabusiness@gmail.com>
2020-09-23 12:27:32 +0000

    apply new layout to player stats page

--- src/client/pages/stats/PlayerPage/index.tsx
+++ src/client/pages/stats/PlayerPage/index.tsx
@@ -1,185 +1,98 @@
 // Library imports
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { Helmet } from 'react-helmet';
 import { useParams } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
 import { IPlayerStatsFormat, Weapon } from 'corehalla.js';
 
 // Hooks
 import { useFetchData } from '../../../hooks/useFetchData';
 import { useMockData } from '../../../hooks/useMockData';
-import { useHashTabs } from '../../../hooks/useHashTabs';
 
-// Components imports
-import { AppBar } from '../../../components/AppBar';
-import { BottomNavigationBar } from '../../../components/BottomNavigationBar';
-import { Loader } from '../../../components/Loader';
-import { Page, PageContentWrapper } from '../../../components/Page';
+// Layout imports
+import { MainLayout } from '../../../layout';
 
 // Tabs imports
 import { OverviewTab } from './OverviewTab';
 import { TeamsTab } from './TeamsTab';
 import { LegendsTab } from './LegendsTab';
 
-type PlayerStatsTab = '#overview' | '#teams' | '#legends' | '#weapons';
-
-const sectionTransition = {
-    in: {
-        opacity: 1,
-    },
-    out: {
-        opacity: 0,
-    },
-    init: {
-        opacity: 0,
-    },
-};
+type PlayerStatsTab = 'overview' | 'teams' | 'legends' | 'weapons';
 
 export const PlayerPage: FC = () => {
-    // Fetch Player ID
+    // Fetch Clan ID
     const { id: playerId } = useParams<{ id: string }>();
-    // Initialize Tabs
-    const [activeTab] = useHashTabs<PlayerStatsTab>(['#overview', '#teams', '#legends', '#weapons'], '#overview');
+
     // Fetch Player Stats
     const [playerStats, loading] =
         process.env.NODE_ENV === 'production'
             ? useFetchData<IPlayerStatsFormat>(`/api/stats/player/${playerId}`)
             : useMockData<IPlayerStatsFormat>('PlayerStats', 0);
 
-    const [activeWeapon, setActiveWeapon] = useState<Weapon | 'all'>('all');
+    const renderOverviewTab = () => <OverviewTab playerStats={playerStats} />;
 
-    const renderActiveTab = () => {
-        switch (activeTab) {
-            case '#teams':
-                return <TeamsTab {...playerStats.season} />;
-            case '#legends':
-                return (
-                    <LegendsTab
-                        legends={
-                            activeWeapon === 'all'
-                                ? playerStats.legends
-                                : playerStats.legends.filter((l) =>
-                                      [l.weapons.weaponOne.name, l.weapons.weaponTwo.name].includes(activeWeapon),
-                                  )
-                        }
-                    />
-                );
-            default:
-                return <OverviewTab playerStats={playerStats} />;
-        }
-    };
+    const renderTeamsTab = () => <TeamsTab {...playerStats.season} />;
+
+    const renderLegendsTab = (activeWeapon: Weapon | 'all') => (
+        <LegendsTab
+            legends={
+                activeWeapon === 'all'
+                    ? playerStats.legends
+                    : playerStats.legends.filter((l) =>
+                          [l.weapons.weaponOne.name, l.weapons.weaponTwo.name].includes(activeWeapon),
+                      )
+            }
+        />
+    );
+
+    const renderWeaponsTab = () => <OverviewTab playerStats={playerStats} />; // TODO: Weapons Tab
 
     return (
-        <Page>
+        <>
             {!loading && (
                 <Helmet>
                     <title>{playerStats.name} Stats • Corehalla</title>
                 </Helmet>
             )}
-            <AppBar
-                title={loading ? 'loading' : playerStats.name}
-                tabs={[
-                    { title: 'overview', link: `#`, active: activeTab === '#overview' },
-                    { title: 'teams', link: `#teams`, active: activeTab === '#teams' },
-                    { title: 'legends', link: `#legends`, active: activeTab === '#legends' },
-                    { title: 'weapons', link: `#weapons`, active: activeTab === '#weapons' },
-                ]}
-                chips={
-                    activeTab === '#legends' && [
-                        {
-                            title: 'All',
-                            action: () => setActiveWeapon('all'),
-                            active: activeWeapon === 'all',
-                        },
-                        {
-                            title: 'Hammer',
-                            action: () => setActiveWeapon('Hammer'),
-                            active: activeWeapon === 'Hammer',
-                        },
-                        {
-                            title: 'Sword',
-                            action: () => setActiveWeapon('Sword'),
-                            active: activeWeapon === 'Sword',
-                        },
-                        {
-                            title: 'Blasters',
-                            action: () => setActiveWeapon('Blasters'),
-                            active: activeWeapon === 'Blasters',
-                        },
-
-                        {
-                            title: 'Rocket Lance',
-                            action: () => setActiveWeapon('Rocket Lance'),
-                            active: activeWeapon === 'Rocket Lance',
-                        },
-                        {
-                            title: 'Spear',
-                            action: () => setActiveWeapon('Spear'),
-                            active: activeWeapon === 'Spear',
-                        },
-                        {
-                            title: 'Katars',
-                            action: () => setActiveWeapon('Katars'),
-                            active: activeWeapon === 'Katars',
-                        },
-                        {
-                            title: 'Axe',
-                            action: () => setActiveWeapon('Axe'),
-                            active: activeWeapon === 'Axe',
-                        },
-                        {
-                            title: 'Bow',
-                            action: () => setActiveWeapon('Bow'),
-                            active: activeWeapon === 'Bow',
-                        },
-                        {
-                            title: 'Gauntlets',
-                            action: () => setActiveWeapon('Gauntlets'),
-                            active: activeWeapon === 'Gauntlets',
-                        },
-                        {
-                            title: 'Scythe',
-                            action: () => setActiveWeapon('Scythe'),
-                            active: activeWeapon === 'Scythe',
-                        },
-                        {
-                            title: 'Cannon',
-                            action: () => setActiveWeapon('Cannon'),
-                            active: activeWeapon === 'Cannon',
-                        },
-                        {
-                            title: 'Orb',
-                            action: () => setActiveWeapon('Orb'),
-                            active: activeWeapon === 'Orb',
-                        },
-                    ]
-                }
-            />
-            <PageContentWrapper pTop={activeTab === '#legends' ? '9rem' : '6rem'} pBtm="3rem">
-                <AnimatePresence exitBeforeEnter initial>
-                    {loading ? (
-                        <Loader key="loader" />
-                    ) : (
-                        <motion.div key="page" animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
-                            <main>
-                                <AnimatePresence exitBeforeEnter initial>
-                                    <motion.div
-                                        key={activeTab}
-                                        animate="in"
-                                        exit="out"
-                                        initial="init"
-                                        variants={sectionTransition}
-                                        transition={{ default: { duration: 0.25, ease: 'easeInOut' } }}
-                                    >
-                                        {renderActiveTab()}
-                                    </motion.div>
-                                </AnimatePresence>
-                            </main>
-                        </motion.div>
-                    )}
-                </AnimatePresence>
-            </PageContentWrapper>
-            <BottomNavigationBar />
-        </Page>
+            <MainLayout<PlayerStatsTab, Weapon | 'all'> // TODO: <,U> U is not type safe
+                title={loading ? 'loading' : playerStats.name || 'Corehalla'}
+                tabs={{
+                    overview: {
+                        render: renderOverviewTab,
+                        displayName: 'Overview',
+                    },
+                    teams: {
+                        render: renderTeamsTab,
+                        displayName: 'Teams',
+                    },
+                    legends: {
+                        render: renderLegendsTab,
+                        displayName: 'Legends',
+                        chips: [
+                            { chipName: 'all', displayName: 'All' },
+                            { chipName: 'Hammer' },
+                            { chipName: 'Sword' },
+                            { chipName: 'Blasters' },
+                            { chipName: 'Rocket Lance' },
+                            { chipName: 'Spear' },
+                            { chipName: 'Katars' },
+                            { chipName: 'Axe' },
+                            { chipName: 'Bow' },
+                            { chipName: 'Gauntlets' },
+                            { chipName: 'Scythe' },
+                            { chipName: 'Cannon' },
+                            { chipName: 'Orb' },
+                            { chipName: 'Greatsword' },
+                        ],
+                        defaultChip: 'all',
+                    },
+                    weapons: {
+                        render: renderWeaponsTab,
+                        displayName: 'Weapons',
+                    },
+                }}
+                defaultTab="overview"
+                loading={loading}
+            ></MainLayout>
+        </>
     );
 };
