e17442b28ccfff69c20e68f152630b08676a5070
Djobbo Maïga <corehallabusiness@gmail.com>
2020-09-24 08:48:45 +0000

    webpack config

---
+++ webpack/client.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const commonConfig = require('./common.config');
+
+module.exports = {
+    ...commonConfig,
+    entry: '../src/client/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'assets/js/bundle.js',
+        publicPath: '/',
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{ from: 'src/public', to: './' }],
+        }),
+        new HtmlWebPackPlugin({
+            template: 'src/client/index.ejs',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+    ],
+};
---
+++ webpack/server.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const commonConfig = require('./common.config');
+
+module.exports = {
+    ...commonConfig,
+    entry: '../src/server/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'functions'),
+        filename: 'functions.js',
+    },
+};
