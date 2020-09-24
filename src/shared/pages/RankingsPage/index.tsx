8d3d06a9e562037cdf020aa22ffee8f93edb2cf4
Djobbo Maïga <corehallabusiness@gmail.com>
2020-09-24 11:53:13 +0000

    migrated rankings page to new layout

--- src/shared/pages/RankingsPage/index.tsx
+++ src/shared/pages/RankingsPage/index.tsx
@@ -1,44 +1,35 @@
 // Library imports
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import { useParams, useLocation } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
-import { IRanking1v1Format, RankedRegion } from 'corehalla.js';
-import qs from 'qs';
+import { useLocation, useParams } from 'react-router-dom';
+import { ClanRank, IClanFormat, IRanking1v1Format, RankedRegion } from 'corehalla.js';
+import loadable from '@loadable/component';
 
 // Hooks
 import { useFetchData } from '../../hooks/useFetchData';
 import { useMockData } from '../../hooks/useMockData';
 
-// Components imports
-import { AppBar } from '../../components/AppBar';
-import { BottomNavigationBar } from '../../components/BottomNavigationBar';
-import { Loader } from '../../components/Loader';
-import { Page, PageContentWrapper } from '../../components/Page';
+// Layout imports
+import { MainLayout } from '../../layout';
+import { NavigationContext } from '../../providers/NavigationProvider';
+import qs from 'qs';
 
 // Tabs imports
-import { Rankings1v1Tab } from './Rankings1v1Tab';
-// import { Rankings2v2Tab } from './Rankings2v2Tab';
-
-import { NavigationContext } from '../../providers/NavigationProvider';
+const Rankings1v1Tab = loadable(() => import('./Rankings1v1Tab'), {
+    resolveComponent: (mod) => mod.Rankings1v1Tab,
+});
+// const Rankings2v2Tab = loadable(() => import('./Rankings2v2Tab'), {
+//     resolveComponent: (mod) => mod.Rankings2v2Tab,
+// });
 
 type Bracket = '1v1' | '2v2' | 'power1v1' | 'power2v2';
 
-const sectionTransition = {
-    in: {
-        opacity: 1,
-    },
-    out: {
-        opacity: 0,
-    },
-    init: {
-        opacity: 0,
-    },
-};
-
 export const RankingsPage: FC = () => {
     const { setActivePage } = useContext(NavigationContext);
-    setActivePage('Rankings');
+
+    useEffect(() => {
+        setActivePage('Rankings');
+    }, []);
 
     // Fetch Rankings Options
     const { bracket = '1v1', region = 'all', page = '1' } = useParams<{
@@ -49,23 +40,16 @@ export const RankingsPage: FC = () => {
     const { search } = useLocation();
     const playerSearch = qs.parse(search.substring(1)).p || '';
 
-    // Fetch Player Stats
+    // Fetch Rankings
     const [rankings, loading] =
         process.env.NODE_ENV === 'production'
             ? useFetchData<IRanking1v1Format[]>(`/api/rankings/1v1/${region}/${page}?p=${playerSearch}`)
             : useMockData<IRanking1v1Format[]>('1v1Rankings', 0);
 
-    const renderActiveTab = () => {
-        switch (bracket) {
-            // case '2v2':
-            //     return <Rankings2v2Tab rankings={rankings} />;
-            default:
-                return <Rankings1v1Tab rankings={rankings} />;
-        }
-    };
+    const renderRankings1v1Tab = () => <Rankings1v1Tab rankings={rankings} />;
 
     return (
-        <Page>
+        <>
             {!loading && (
                 <Helmet>
                     <title>
@@ -73,7 +57,7 @@ export const RankingsPage: FC = () => {
                     </title>
                 </Helmet>
             )}
-            <AppBar
+            <MainLayout<Bracket, RankedRegion>
                 title={
                     loading
                         ? 'loading'
@@ -81,78 +65,16 @@ export const RankingsPage: FC = () => {
                               region.toUpperCase() === 'ALL' ? '' : `${region.toUpperCase()} `
                           }${bracket} Rankings - Page ${page}`
                 }
-                tabs={[
-                    { title: '1v1', link: `/rankings/1v1/${region}/1`, active: bracket === '1v1' },
-                    { title: '2v2', link: `/rankings/2v2/${region}/1`, active: bracket === '2v2' },
-                    { title: 'Power 1v1', link: `/rankings/power1v1/${region}/1`, active: bracket === 'power1v1' },
-                    { title: 'Power 2v2', link: `/rankings/power2v2/${region}/1`, active: bracket === 'power2v2' },
-                ]}
-                chips={[
-                    {
-                        title: 'All',
-                        link: `/rankings/${bracket}/all/1`,
-                        active: region === 'all',
+                tabs={{
+                    ['1v1']: {
+                        render: renderRankings1v1Tab,
+                        displayName: 'Ranked 1v1',
+                        link: `/rankings/1v1/${region}/1`,
                     },
-                    {
-                        title: 'US-E',
-                        link: `/rankings/${bracket}/us-e/1`,
-                        active: region === 'us-e',
-                    },
-                    {
-                        title: 'EU',
-                        link: `/rankings/${bracket}/eu/1`,
-                        active: region === 'eu',
-                    },
-                    {
-                        title: 'SEA',
-                        link: `/rankings/${bracket}/sea/1`,
-                        active: region === 'sea',
-                    },
-                    {
-                        title: 'BRZ',
-                        link: `/rankings/${bracket}/brz/1`,
-                        active: region === 'brz',
-                    },
-                    {
-                        title: 'AUS',
-                        link: `/rankings/${bracket}/aus/1`,
-                        active: region === 'aus',
-                    },
-                    {
-                        title: 'US-W',
-                        link: `/rankings/${bracket}/us-w/1`,
-                        active: region === 'us-w',
-                    },
-                    {
-                        title: 'JPN',
-                        link: `/rankings/${bracket}/jpn/1`,
-                        active: region === 'jpn',
-                    },
-                ]}
-            />
-            <AnimatePresence exitBeforeEnter initial>
-                {loading ? (
-                    <Loader key="loader" />
-                ) : (
-                    <motion.div key="page" animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
-                        <main>
-                            <AnimatePresence exitBeforeEnter initial>
-                                <motion.div
-                                    key={bracket}
-                                    animate="in"
-                                    exit="out"
-                                    initial="init"
-                                    variants={sectionTransition}
-                                    transition={{ default: { duration: 0.25, ease: 'easeInOut' } }}
-                                >
-                                    {renderActiveTab()}
-                                </motion.div>
-                            </AnimatePresence>
-                        </main>
-                    </motion.div>
-                )}
-            </AnimatePresence>
-            <BottomNavigationBar />
-        </Page>
+                }}
+                defaultTab="1v1"
+                loading={loading}
+            ></MainLayout>
+        </>
     );
 };
