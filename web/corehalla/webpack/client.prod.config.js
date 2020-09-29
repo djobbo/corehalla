328da52f1ace4dbbe89e48749c2a1156efbdf81d
Djobbo Maïga <corehallabusiness@gmail.com>
2020-09-29 11:53:28 +0000

    temp ssr removal

--- web/corehalla/webpack/client.prod.config.js
+++ web/corehalla/webpack/client.prod.config.js
@@ -1,6 +1,17 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 const commonClientConfig = require('./client.common.config');
 
 module.exports = {
     ...commonClientConfig,
     mode: 'production',
+    plugins: [
+        // TODO: Remove this
+        ...commonClientConfig.plugins,
+        new HtmlWebPackPlugin({
+            template: 'src/client/index.ejs',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+    ],
 };
