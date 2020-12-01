3cb75a8feb12abd21de57c4fd375813e8426beba
Alfie <djobbo.maiga@gmail.com>
2020-12-01 07:43:20 +0100

    nginx+api

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -40,8 +40,10 @@ module.exports = {
         }),
     ],
     devServer: {
+        host: '0.0.0.0',
         port: 3000,
         historyApiFallback: true,
         hot: true,
+        publicPath: '/dist',
     },
 };
