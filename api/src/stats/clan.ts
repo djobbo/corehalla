71b1d00a9a38822e50b2de10560836659f912b06
Alfie <djobbo.maiga@gmail.com>
2020-12-01 09:46:39 +0100

    rm api cache ctrl

--- api/src/stats/clan.ts
+++ api/src/stats/clan.ts
@@ -3,11 +3,9 @@ import { bhAPI } from '../bhAPI';
 
 const router = express.Router();
 
-const apiCacheControl = `public, max-age=180, s-maxage=240`;
-
 router.get('/:id', (req, res) => {
 	const id = parseInt(req.params.id);
-	res.set('Cache-Control', apiCacheControl);
+
 	bhAPI
 		.fetchClanFormat(id)
 		.then(res.status(200).send)
