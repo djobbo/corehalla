cc53019aa97c832582028a739ea742f9922445b2
Alfie <djobbo.maiga@gmail.com>
2021-02-16 20:18:23 +0100

    brynn landing + landing navbar fix

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -10,28 +10,24 @@ import { StatSmall, StatDesc } from '@components/TextStyles';
 import { PlayerSearchContext } from '@providers/PlayerSearchProvider';
 import { useViewportScroll } from 'framer-motion';
 import { SideNavLayout } from '@layout/SideNavLayout';
-import { MenuIcon } from '@components/Icons';
 import { SideNavContext } from '@providers/SideNavProvider';
 
 export default function HomePage() {
 	const { scrollY } = useViewportScroll();
 	const { setPlayerSearch } = useContext(PlayerSearchContext);
 	const [hasScrolled, setHasScrolled] = useState(scrollY.get() > 0);
-	const { setSideNavOpen } = useContext(SideNavContext);
+	const { sideNavOpen } = useContext(SideNavContext);
 
 	scrollY.onChange(() => {
 		setHasScrolled(scrollY.get() > 0);
 	});
 
 	return (
-		<SideNavLayout>
-			<Head>
-				<title>Index • Corehalla</title>
-			</Head>
+		<>
 			<div
 				className={`${styles.landingNavbar} ${
 					hasScrolled ? styles.hasScrolled : ''
-				}`}
+				} ${sideNavOpen ? styles.sideNavOpen : ''}`}
 			>
 				<Link href='/'>
 					<a>
@@ -51,41 +47,45 @@ export default function HomePage() {
 					</Link>
 				</ul>
 			</div>
-			<main className={layoutStyles.container}>
-				<div className={styles.landingSections}>
-					<div className={styles.landingContent}>
-						<div className={styles.text}>
-							<h1 className={styles.title}>
-								Stats, to the core.
-							</h1>
-							<p className={styles.desc}>
-								Corehalla is a statistics website for the game
-								Brawlhalla.
-								<br />
-								Designed to be as intuitive as possible, it
-								gives players all the information they need
-								regarding the ranked leaderboard, power rankings
-								and specific players & clans stats.
-							</p>
-							<input
-								className={styles.searchInput}
-								type='text'
-								onChange={(e) =>
-									setPlayerSearch(e.target.value)
-								}
-								placeholder='Search Player...'
-							/>
+			<SideNavLayout>
+				<Head>
+					<title>Index • Corehalla</title>
+				</Head>
+				<main className={layoutStyles.container}>
+					<div className={styles.landingSections}>
+						<div className={styles.landingContent}>
+							<div className={styles.text}>
+								<h1 className={styles.title}>
+									Stats, to the core.
+								</h1>
+								<p className={styles.desc}>
+									Corehalla is a statistics website for the
+									game Brawlhalla.
+									<br />
+									Designed to be as intuitive as possible, it
+									gives players all the information they need
+									regarding the ranked leaderboard, power
+									rankings and specific players & clans stats.
+								</p>
+								<input
+									className={styles.searchInput}
+									type='text'
+									onChange={(e) =>
+										setPlayerSearch(e.target.value)
+									}
+									placeholder='Search Player...'
+								/>
+							</div>
+							<div className={styles.landingImg}>
+								<img
+									src='/images/Brynn_BP3_Landing.gif'
+									alt=''
+									width='50%'
+									height='auto'
+								/>
+							</div>
 						</div>
-						<div className={styles.landingImg}>
-							<img
-								src='/images/Bodvar Landing.png'
-								alt=''
-								width='50%'
-								height='auto'
-							/>
-						</div>
-					</div>
-					{/* <div className={`${styles.landingContent} ${styles.right}`}>
+						{/* <div className={`${styles.landingContent} ${styles.right}`}>
 					<div className={styles.text}>
 						<h1 className={styles.title}>Find your clan</h1>
 						<p className={styles.desc}>XD</p>
@@ -106,39 +106,40 @@ export default function HomePage() {
 					</div>
 				</div> */}
 
-					<div className={styles.mainContent}>
-						<div className={styles.blips}>
-							<div className={styles.blip}>
-								<StatSmall>Rankings</StatSmall>
-								<br />
-								<StatDesc>
-									Lorem ipsum dolor sit, amet consectetur
-									adipisicing elit. At, itaque excepturi eius
-									repellendus atque.
-								</StatDesc>
-							</div>
-							<div className={styles.blip}>
-								<StatSmall>Player Stats</StatSmall>
-								<br />
-								<StatDesc>
-									Lorem ipsum dolor, sit amet consectetur
-									adipisicing elit. Vel perspiciatis incidunt
-									odio sit.
-								</StatDesc>
-							</div>
-							<div className={styles.blip}>
-								<StatSmall>Clan Stats</StatSmall>
-								<br />
-								<StatDesc>
-									Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Temporibus facere natus
-									animi ut.
-								</StatDesc>
+						<div className={styles.mainContent}>
+							<div className={styles.blips}>
+								<div className={styles.blip}>
+									<StatSmall>Rankings</StatSmall>
+									<br />
+									<StatDesc>
+										Lorem ipsum dolor sit, amet consectetur
+										adipisicing elit. At, itaque excepturi
+										eius repellendus atque.
+									</StatDesc>
+								</div>
+								<div className={styles.blip}>
+									<StatSmall>Player Stats</StatSmall>
+									<br />
+									<StatDesc>
+										Lorem ipsum dolor, sit amet consectetur
+										adipisicing elit. Vel perspiciatis
+										incidunt odio sit.
+									</StatDesc>
+								</div>
+								<div className={styles.blip}>
+									<StatSmall>Clan Stats</StatSmall>
+									<br />
+									<StatDesc>
+										Lorem ipsum dolor sit amet consectetur
+										adipisicing elit. Temporibus facere
+										natus animi ut.
+									</StatDesc>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</main>
-		</SideNavLayout>
+				</main>
+			</SideNavLayout>
+		</>
 	);
 }
