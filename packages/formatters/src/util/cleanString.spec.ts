10ee5f263bf70d5e32bbe6a9b28dcbee6ab34499
Alfie <djobbo.maiga@gmail.com>
2021-04-29 00:42:30 +0200

    fix

---
+++ packages/formatters/src/util/cleanString.spec.ts
@@ -0,0 +1,8 @@
+import { cleanString } from './cleanString';
+
+describe('util/cleanString', () => {
+    it('cleans string', () => {
+        //TODO: BETTER TEST
+        expect(cleanString('abcde123')).toBe('abcde123');
+    });
+});
