6295fb2e2f9a0c79346f30b885d4f719186b93a1
Alfie <djobbo.maiga@gmail.com>
2021-04-30 22:48:50 +0200

    article layout

---
+++ apps/web/stats/src/components/ArticleNavbar/index.tsx
@@ -0,0 +1,37 @@
+import styles from './index.module.scss';
+import { useState } from 'react';
+import Link from 'next/link';
+import { useViewportScroll } from 'framer-motion';
+import { useSideNavContext } from '~providers/SideNavProvider';
+
+export const ArticleNavbar = (): JSX.Element => {
+    const { scrollY } = useViewportScroll();
+    const [hasScrolled, setHasScrolled] = useState(scrollY.get() > 0);
+    const { sideNavOpen } = useSideNavContext();
+
+    scrollY.onChange(() => {
+        setHasScrolled(scrollY.get() > 0);
+    });
+
+    return (
+        <div
+            className={`${styles.wrapper} ${hasScrolled ? styles.hasScrolled : ''} ${
+                sideNavOpen ? styles.sideNavOpen : ''
+            }`}
+        >
+            <Link href="/">
+                <a>
+                    <img className={styles.mainLogo} src="/images/logo.png" alt="Corehalla Logo" />
+                </a>
+            </Link>
+            <ul>
+                <Link href="/">Home</Link>
+                <Link href="/rankings">Rankings</Link>
+                <Link href="/favorites">Favorites</Link>
+                <Link href="#">
+                    <a className={styles.navCTA}>Login</a>
+                </Link>
+            </ul>
+        </div>
+    );
+};
