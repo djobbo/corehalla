e83055fbcfd563eb41a07a21decea94dfa60b91e
Alfie <djobbo.maiga@gmail.com>
2021-05-11 06:36:36 +0200

    fancy discord

--- apps/discord/una/src/RankEmbed.tsx
+++ apps/discord/una/src/RankEmbed.tsx
@@ -1,16 +1,5 @@
-import { IPlayerStatsFormat } from '@corehalla/types';
-import {
-    Author,
-    Break,
-    Description,
-    EmbedWrapper,
-    Field,
-    React,
-    Span,
-    Timestamp,
-    Title,
-    Wrapper,
-} from 'fancy-discord.js/lib/Embeds';
+import { ILegendStatsFormat, IPlayerStatsFormat } from '@corehalla/types';
+import { React } from 'fancy-discord.js/lib/Embeds';
 
 interface Props {
     type: 'stats' | 'ranked';
@@ -18,40 +7,70 @@ interface Props {
     iconUrl?: string | null;
 }
 
-export const RankEmbed = ({ playerStats: { name, season, wins, games }, iconUrl, type }: Props): EmbedWrapper => {
+// TODO: Define JSX.Element in fancy-discord.js
+const LegendsRankedField = ({ legends }: { legends: ILegendStatsFormat[] }): JSX.Element => {
+    const highestLegend = legends.sort((a, b) => b.season.rating - a.season.rating)[0];
+    const mostPlayedLegend = legends.sort((a, b) => b.season.games - a.season.games)[0];
+
     return (
-        <Wrapper>
-            <Title>
-                {type === 'stats' ? '📈' : '🥇'} {name}
-            </Title>
-            <Author name="Corehalla" iconURL={iconUrl ?? undefined} />
-            <Description>
-                React with {type === 'ranked' ? '📈 to view general stats' : '🥇 to view current season stats'}
-            </Description>
+        <field title="Legends" inline>
+            <span bold>Highest Rating: </span>
+            {highestLegend.name} ({highestLegend.season.rating} Elo)
+            <br />
+            <span bold>Most Played: </span>
+            {mostPlayedLegend.name} ({highestLegend.season.games} Games)
+        </field>
+    );
+};
+
+// FIXME: null elements
+export const RankEmbed = ({
+    playerStats: { name, season, wins, games, clan, legends },
+    iconUrl,
+    type,
+}: Props): JSX.Element => {
+    return (
+        <>
+            <title>{type === 'stats' ? '📈 Player Stats' : '🥇 Ranked Data'}</title>
+            <author iconURL={iconUrl ?? undefined}>Corehalla</author>
+            <desc>React with {type === 'ranked' ? '📈 to view general stats' : '🥇 to view current season stats'}</desc>
             {type === 'ranked' ? (
-                <Wrapper>
-                    <Field title="Region" inline>
+                <>
+                    <field title="Name" inline>
+                        <span bold>{name}</span>
+                        <br />
+                        {clan ? (
+                            <span>
+                                {'< '}
+                                <link href={`https://neue.corehalla.com/stats/clan/${clan.id}`}>{clan.name}</link>
+                                {' >'}
+                            </span>
+                        ) : (
+                            ''
+                        )}
+                    </field>
+                    <field title="Region" inline>
                         {season.region}
-                    </Field>
-                    <Field title="1v1 Rating" inline>
-                        {/* FIXME: allow numbers */}
-                        <Span bold>{season.tier}</Span> ({season.rating.toString()} / {season.peak.toString()} Peak)
-                        <Break />
+                    </field>
+                    <field title="1v1 Rating" inline>
+                        <span bold>{season.tier}</span> ({season.rating.toString()} / {season.peak.toString()} Peak)
+                        <br />
                         {season.wins.toString()} Wins / {(season.games - season.wins).toString()} Losses (
                         {season.games.toString()} Games)
-                        <Break />
+                        <br />
                         {(season.wins / season.games).toFixed(2)}% Winrate
-                    </Field>
-                </Wrapper>
+                    </field>
+                    <LegendsRankedField legends={legends} />
+                </>
             ) : (
-                <Field title="Games">
+                <field title="Games">
                     {wins.toString()} Wins / {(games - wins).toString()} Losses ({games.toString()} Games)
-                    <Break />
-                    {(wins / season.games).toFixed(2)}% Winrate
-                </Field>
+                    <br />
+                    {(wins / games).toFixed(2)}% Winrate
+                </field>
             )}
-            <Timestamp timestamp={Date.now()} />
+            <timestamp timestamp={Date.now()} />
             {/* FIXME: <Url>asdas</Url> */}
-        </Wrapper>
+        </>
     );
 };
