b942476748d3a12d914444eb1078277afeef7671
Alfie <4998293+AlfieGoldson@users.noreply.github.com>
2021-07-18 13:57:46 +0200

    searchbar

--- www/stats/src/components/SearchBar/SearchBar.tsx
+++ www/stats/src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,9 @@
-import { Select } from '@Select'
+import { Option, Select } from '@Select'
+import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
 import { useDebounceValue } from '~hooks/useDebounce'
+import { IRanking1v1Format } from '../../../../../packages/core/types'
 
 import styles from './SearchBar.module.scss'
 
@@ -20,8 +22,24 @@ export const SearchBar = (): JSX.Element => {
     const [val, setVal, isLoading] = useDebounceValue('', 500)
     const [options, setOptions] = useState([])
 
+    const router = useRouter()
+
+    const handleChange = (playerId: string) => {
+        console.log({ playerId })
+        if (!playerId) return
+
+        router.push(`/stats/player/${playerId}`)
+    }
+
     useEffect(() => {
         if (!val) return
+        ;(async () => {
+            try {
+                const res = await fetch('/api/rankings/1v1/ALL/1')
+                const data = (await res.json()) as IRanking1v1Format[]
+                setOptions(data.map<Option<string>>((player) => ({ value: player.id.toString(), label: player.name })))
+            } catch {}
+        })()
 
         setOptions([{ value: '1213456', label: val }])
     }, [val])
@@ -32,7 +50,7 @@ export const SearchBar = (): JSX.Element => {
                 options={options}
                 onInputChange={setVal}
                 placeholder="Search Player..."
-                onChange={(value) => console.log({ value })}
+                onChange={handleChange}
                 searchable
                 clearable
             />
