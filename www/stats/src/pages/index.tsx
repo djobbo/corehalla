21e5d182813ff372e72a2804d1cd1853ea7bde15
Alfie <4998293+AlfieGoldson@users.noreply.github.com>
2021-07-19 11:22:58 +0200

    og image

--- www/stats/src/pages/index.tsx
+++ www/stats/src/pages/index.tsx
@@ -13,7 +13,7 @@ const HomePage = (): JSX.Element => {
     return (
         <>
             <Head>
-                <title>Index • Corehalla</title>
+                <title>Home • Corehalla</title>
             </Head>
             <Header />
             <div className={styles.landingSections}>
