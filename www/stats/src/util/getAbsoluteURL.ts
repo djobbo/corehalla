469507e19e6a7722ee536e19e7fd9590bde13123
Alfie <4998293+AlfieGoldson@users.noreply.github.com>
2021-08-08 20:08:26 +0200

    prod url

--- www/stats/src/util/getAbsoluteURL.ts
+++ www/stats/src/util/getAbsoluteURL.ts
@@ -1,4 +1,8 @@
 export const getAbsoluteURL = (path: string): string => {
-    const baseURL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
+    const baseURL = process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : process.env.NODE_ENV === 'production'
+        ? process.env.PROD_URL
+        : 'http://localhost:3000'
     return baseURL + path
 }
