596e8b73c641654b74feba6dc59515fce2cb8978
Alfie <4998293+AlfieGoldson@users.noreply.github.com>
2021-08-08 20:14:08 +0200

    NEXT_PUBLIC_

--- www/stats/src/util/getAbsoluteURL.ts
+++ www/stats/src/util/getAbsoluteURL.ts
@@ -2,7 +2,7 @@ export const getAbsoluteURL = (path: string): string => {
     const baseURL = process.env.VERCEL_URL
         ? `https://${process.env.VERCEL_URL}`
         : process.env.NODE_ENV === 'production'
-        ? process.env.PROD_URL
+        ? process.env.NEXT_PUBLIC_PROD_URL
         : 'http://localhost:3000'
     return baseURL + path
 }
