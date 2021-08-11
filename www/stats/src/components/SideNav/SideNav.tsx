ce0df348c11c68733caf7026429bdf95254a484d
Alfie <4998293+AlfieGoldson@users.noreply.github.com>
2021-08-11 20:17:07 +0200

    search provider

--- www/stats/src/components/SideNav/SideNav.tsx
+++ www/stats/src/components/SideNav/SideNav.tsx
@@ -1,3 +1,4 @@
+import { legends } from '@corehalla/core/static'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
@@ -5,11 +6,11 @@ import styles from './SideNav.module.scss'
 
 import { useAuth } from '~providers/AuthProvider'
 import { useFavorites } from '~providers/FavoritesProvider'
+import { useSearch } from '~providers/SearchProvider'
 
 import { FavoriteIcon, HomeIcon, RankingsIcon, SettingsIcon } from '@Icons'
 
 import { signIn, signOut } from '~supabase/client'
-import { useSearch } from '~providers/SearchProvider'
 
 interface BottomNavigationTab {
     title: string
@@ -48,7 +49,7 @@ export const SideNav = (): JSX.Element => {
 
     const { user } = useAuth()
 
-    const { search, setSearch } = useSearch()
+    const { search, setSearch, rankings } = useSearch()
 
     if (typeof document === 'undefined') return null
 
@@ -67,28 +68,54 @@ export const SideNav = (): JSX.Element => {
                         </a>
                     </Link>
                 ))}
+                {favorites.length > 0 && <hr className={styles.separator} />}
+                <input type="text" value={search} onChange={({ target }) => setSearch(target.value)} />
                 {favorites.length > 0 && (
                     <>
-                        <hr className={styles.separator} />
-                        <input type="text" value={search} onChange={({ target }) => setSearch(target.value)} />
+                        <p>Favorites</p>
+                        {favorites
+                            .filter((fav) => fav.label.toLowerCase().includes(search.toLowerCase()))
+                            .map(({ favorite_id, type, label, thumb_uri }) => (
+                                <Link href={`/stats/${type}/${favorite_id}`} key={`${type}/${favorite_id}`}>
+                                    <a
+                                        className={`${styles.navItem} ${
+                                            pathname.startsWith(`/stats/${type}`) && query.id === favorite_id
+                                                ? styles.active
+                                                : ''
+                                        }`}
+                                    >
+                                        <img src={thumb_uri} alt={label} />
+                                        <span>{label.substr(0, 16)}</span>
+                                    </a>
+                                </Link>
+                            ))}
                     </>
                 )}
-                {favorites
-                    .filter((fav) => fav.label.toLowerCase().includes(search.toLowerCase()))
-                    .map(({ favorite_id, type, label, thumb_uri }) => (
-                        <Link href={`/stats/${type}/${favorite_id}`} key={`${type}/${favorite_id}`}>
-                            <a
-                                className={`${styles.navItem} ${
-                                    pathname.startsWith(`/stats/${type}`) && query.id === favorite_id
-                                        ? styles.active
-                                        : ''
-                                }`}
-                            >
-                                <img src={thumb_uri} alt={label} />
-                                <span>{label.substr(0, 16)}</span>
-                            </a>
-                        </Link>
-                    ))}
+                {rankings.length > 0 && (
+                    <>
+                        <p>Rankings</p>
+                        {rankings.map(({ name, bestLegend, id }) => (
+                            <Link href={`/stats/player/${id}`} key={`player/${id}`}>
+                                <a
+                                    className={`${styles.navItem} ${
+                                        pathname.startsWith(`/stats/player`) && query.id === id.toString()
+                                            ? styles.active
+                                            : ''
+                                    }`}
+                                >
+                                    {/* @corehalla/core bestLegend.name */}
+                                    <img
+                                        src={`/images/icons/legends/${
+                                            legends.find((l) => l.id === bestLegend.id).name
+                                        }.png`}
+                                        alt={bestLegend.id.toString()}
+                                    />
+                                    <span>{name.substr(0, 16)}</span>
+                                </a>
+                            </Link>
+                        ))}
+                    </>
+                )}
             </div>
             <div className={styles.content}>
                 {user ? (
--- www/stats/src/providers/SearchProvider.tsx
+++ www/stats/src/providers/SearchProvider.tsx
@@ -1,8 +1,8 @@
+import { IRanking1v1Format } from '@corehalla/core/types'
 import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useState } from 'react'
 
 import { useDebounceValue } from '~hooks/useDebounce'
-import { IRanking1v1Format } from '@corehalla/core/types'
-import { useFavorites } from './FavoritesProvider'
+
 import { Option } from '@Select'
 
 interface SearchContext {
@@ -11,6 +11,7 @@ interface SearchContext {
     setSearch: Dispatch<SetStateAction<string>>
     isLoading: boolean
     options: Option<string>[]
+    rankings: IRanking1v1Format[]
 }
 
 const searchContext = createContext<SearchContext>({
@@ -19,6 +20,7 @@ const searchContext = createContext<SearchContext>({
     setSearch: () => ({}),
     isLoading: false,
     options: [],
+    rankings: [],
 })
 
 export const useSearch = (): SearchContext => useContext(searchContext)
@@ -29,14 +31,9 @@ interface Props {
 
 export const SearchProvider = ({ children }: Props): JSX.Element => {
     const [search, debouncedSearch, setSearch, isLoading] = useDebounceValue('', 500)
-
-    const { favorites } = useFavorites()
-
-    const [options, setOptions] = useState<Option<string>[]>(
-        favorites
-            .filter((fav) => fav.type === 'player')
-            .map((player) => ({ value: player.favorite_id, label: player.label })),
-    )
+    const [options, setOptions] = useState<Option<string>[]>([])
+    const [rankings, setRankings] = useState<IRanking1v1Format[]>([])
+    const [filteredRankings, setFilteredRankings] = useState<IRanking1v1Format[]>([])
 
     useEffect(() => {
         if (!debouncedSearch) return
@@ -44,13 +41,32 @@ export const SearchProvider = ({ children }: Props): JSX.Element => {
             try {
                 const res = await fetch(`/api/rankings/1v1/ALL/1?p=${debouncedSearch}`)
                 const data = (await res.json()) as IRanking1v1Format[]
-                setOptions(data.map<Option<string>>((player) => ({ value: player.id.toString(), label: player.name })))
+                setRankings(data)
             } catch {}
         })()
     }, [debouncedSearch])
 
+    useEffect(() => {
+        setFilteredRankings(
+            !search
+                ? rankings
+                : rankings.filter((ranking) => ranking.name.toLowerCase().includes(search.toLowerCase())),
+        )
+    }, [rankings, search])
+
+    useEffect(() => {
+        setOptions(
+            filteredRankings.map<Option<string>>((player) => ({
+                value: player.id.toString(),
+                label: player.name,
+            })),
+        )
+    }, [filteredRankings])
+
     return (
-        <searchContext.Provider value={{ search, debouncedSearch, setSearch, isLoading, options }}>
+        <searchContext.Provider
+            value={{ rankings: filteredRankings, search, debouncedSearch, setSearch, isLoading, options }}
+        >
             {children}
         </searchContext.Provider>
     )
