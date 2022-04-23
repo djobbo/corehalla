58162a586a6f31148ff4dc20623144101a5b3bea
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-04-23 17:27:08 +0200

    rankings optional catch alls

--- corehalla/app/pages/rankings/_middleware.tsx
+++ corehalla/app/pages/rankings/_middleware.tsx
@@ -3,9 +3,7 @@ import type { NextMiddleware } from "next/server"
 const handler: NextMiddleware = (req) => {
     if (!req.page.name) {
         const url = req.nextUrl.clone()
-        url.pathname = req.nextUrl.pathname.startsWith("/rankings/2v2")
-            ? "/rankings/2v2/all/1"
-            : "/rankings/1v1/all/1"
+        url.pathname = "/rankings/1v1"
         return NextResponse.redirect(url)
     }
 
