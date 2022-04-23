c75fa72e803cadcde0f83d618864dd3be9e0541b
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-04-23 20:08:29 +0200

    pagination

--- corehalla/app/pages/rankings/2v2/[[...rankingsOptions]].tsx
+++ corehalla/app/pages/rankings/2v2/[[...rankingsOptions]].tsx
@@ -1,3 +1,4 @@
+import { Pagination } from "ui/base/Pagination"
 import { RankingsTableItem } from "ui/stats/RankingsTableItem"
 import { Spinner } from "ui/base/Spinner"
 import { cleanString } from "common/helpers/cleanString"
@@ -12,23 +13,29 @@ const Page: NextPage = () => {
 
     const { rankingsOptions } = router.query
 
-    const [bracket, region, page] = Array.isArray(rankingsOptions)
+    const [region = "all", page = "1"] = Array.isArray(rankingsOptions)
         ? rankingsOptions
         : []
 
     const { rankings2v2, isLoading, isError } = useRankings2v2(
         // @ts-expect-error TODO: Typecheck this
-        bracket,
+        "2v2",
         region,
         page,
     )
 
-    if (isLoading) return <Spinner size="4rem" />
-
-    if (isError || !rankings2v2) return <div>Error</div>
+    if (isError || (!isLoading && !rankings2v2)) return <div>Error</div>
 
     return (
-        <div>
+        <>
+            <Pagination
+                getPageHref={(page) =>
+                    `/rankings/2v2/${region ?? "all"}/${page}`
+                }
+                currentPage={parseInt(page)}
+                firstPage={1}
+                className="justify-end"
+            />
             <div className="py-4 w-full h-full flex items-center gap-4">
                 <p className="w-16 text-center">Rank</p>
                 <p className="w-16 text-center">Region</p>
@@ -39,37 +46,47 @@ const Page: NextPage = () => {
                 <p className="w-20 text-center">Winrate</p>
                 <p className="w-40 text-center">Elo</p>
             </div>
-            <div>
-                {rankings2v2.map((team, i) => {
-                    const [player1, player2] = getTeamPlayers(team)
-                    return (
-                        <RankingsTableItem
-                            key={`${player1.id}-${player2.id}`}
-                            index={i}
-                            content={
-                                <>
-                                    <p className="flex flex-1 items-center">
-                                        <Link
-                                            href={`/stats/player/${player1.id}`}
-                                        >
-                                            <a>{cleanString(player1.name)}</a>
-                                        </Link>
-                                    </p>
-                                    <p className="flex flex-1 items-center">
-                                        <Link
-                                            href={`/stats/player/${player2.id}`}
-                                        >
-                                            <a>{cleanString(player2.name)}</a>
-                                        </Link>
-                                    </p>
-                                </>
-                            }
-                            {...team}
-                        />
-                    )
-                })}
-            </div>
-        </div>
+            {isLoading ? (
+                <div className="flex items-center justify-center h-48">
+                    <Spinner size="4rem" />
+                </div>
+            ) : (
+                <div>
+                    {rankings2v2?.map((team, i) => {
+                        const [player1, player2] = getTeamPlayers(team)
+                        return (
+                            <RankingsTableItem
+                                key={`${player1.id}-${player2.id}`}
+                                index={i}
+                                content={
+                                    <>
+                                        <p className="flex flex-1 items-center">
+                                            <Link
+                                                href={`/stats/player/${player1.id}`}
+                                            >
+                                                <a>
+                                                    {cleanString(player1.name)}
+                                                </a>
+                                            </Link>
+                                        </p>
+                                        <p className="flex flex-1 items-center">
+                                            <Link
+                                                href={`/stats/player/${player2.id}`}
+                                            >
+                                                <a>
+                                                    {cleanString(player2.name)}
+                                                </a>
+                                            </Link>
+                                        </p>
+                                    </>
+                                }
+                                {...team}
+                            />
+                        )
+                    })}
+                </div>
+            )}
+        </>
     )
 }
 
