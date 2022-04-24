d3df127e50f932e15bed9afc4c39eb1d8ebceca9
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-04-24 15:30:53 +0200

    landing background

--- corehalla/packages/ui/layout/Layout.tsx
+++ corehalla/packages/ui/layout/Layout.tsx
@@ -1,34 +1,48 @@
 import { Footer } from "./Footer"
 import { Header } from "./Header"
+import { LandingBackground } from "./LandingBackground"
 import { SideNav } from "./SideNav"
+import { bgVar1 } from "../theme/theme"
 import { styled } from "../theme"
 import type { ReactNode } from "react"
 
 export type LayoutProps = {
     children: ReactNode
-    landingBackgroundImg?: string | null
+    showBackground?: boolean
 }
 
-export const Layout = ({ children, landingBackgroundImg }: LayoutProps) => {
-    const LandingBackground = styled("div", {
-        backgroundImage: `url(${landingBackgroundImg})`,
-        opacity: "0.04",
-        filter: "saturate(0.1)",
-        zIndex: -1,
-        maxHeight: "100vh",
-    })
+const BackgroundContainer = styled("div", {
+    "&>svg": {
+        maskImage: `linear-gradient(0deg, ${bgVar1}00 0%, ${bgVar1} 40%)`,
+    },
+})
+
+export const Layout = ({ children, showBackground }: LayoutProps) => {
+    // const LandingBackground = styled("div", {
+    //     backgroundImage: `url(${landingBackgroundImg})`,
+    //     opacity: "0.04",
+    //     filter: "saturate(0.1)",
+    //     zIndex: -1,
+    //     maxHeight: "100vh",
+    // })
 
     return (
-        <div className="relative z-10 flex">
-            <SideNav />
-            <div className="overflow-y-auto flex-1 mx-4 xl:mx-0">
-                <Header />
-                {landingBackgroundImg && (
-                    <LandingBackground className="absolute top-0 inset-x-0 -bottom-8" />
-                )}
-                <div className="flex-1 max-w-screen-xl mx-auto">{children}</div>
-                <Footer />
+        <>
+            {showBackground && (
+                <BackgroundContainer className="w-full h-screen absolute opacity-50">
+                    <LandingBackground className="w-full h-5/6" />
+                </BackgroundContainer>
+            )}
+            <div className="relative z-10 flex">
+                <SideNav />
+                <div className="overflow-y-auto flex-1 mx-4 xl:mx-0">
+                    <Header />
+                    <div className="flex-1 max-w-screen-xl mx-auto">
+                        {children}
+                    </div>
+                    <Footer />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
