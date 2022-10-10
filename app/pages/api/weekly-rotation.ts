5dc8ae9cb79fbc2d5dede3ca258938d964ca1223
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-10-10 12:20:11 +0200

    update cache control

--- app/pages/api/weekly-rotation.ts
+++ app/pages/api/weekly-rotation.ts
@@ -2,6 +2,11 @@ import { getWeeklyRotation } from "web-parser/weekly-rotation/getWeeklyRotation"
 import type { NextApiHandler } from "next"
 
 const handler: NextApiHandler = async (req, res) => {
+    res.setHeader(
+        "Cache-Control",
+        "public, s-maxage=3600, stale-while-revalidate=7200",
+    )
+
     const weeklyRotation = await getWeeklyRotation()
 
     res.status(200).json(weeklyRotation)
