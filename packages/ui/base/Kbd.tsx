cb9219265352b6c64058cb6ae5f6f98a2d457048
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-10-14 16:12:03 +0200

    visual fixes

--- packages/ui/base/Kbd.tsx
+++ packages/ui/base/Kbd.tsx
@@ -16,7 +16,7 @@ export const Kbd = ({ children, className }: KbdProps) => (
     <kbd
         className={cn(
             kbdClassName,
-            "rounded-md flex items-center justify-center px-2 bg-bg",
+            "rounded-md hidden hashover:flex items-center justify-center px-2 bg-bg",
             className,
         )}
     >
