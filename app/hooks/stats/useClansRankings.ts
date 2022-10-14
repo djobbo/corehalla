45ffc33e68c8f611b63bb7237dd0452e32f3082c
Djobbo <hi@djobbo.com>
2022-10-14 20:39:38 +0200

    clans search

--- app/hooks/stats/useClansRankings.ts
+++ app/hooks/stats/useClansRankings.ts
@@ -1,19 +1,17 @@
 import { useQuery } from "react-query"
 import axios from "axios"
-import type { Clan } from "bhapi/types"
+import type { BHClan } from "db/generated/client"
 
 export const useClansRankings = (page = "1", name = "") => {
     const { data: clans, ...query } = useQuery(
         ["clansRankings", page, name],
         async () => {
-            const { data } = await axios.get<Omit<Clan, "clan">[]>(
-                `/api/clans/${page}`,
-                {
-                    params: {
-                        name,
-                    },
+            const { data } = await axios.get<BHClan[]>(`/api/rankings/clans`, {
+                params: {
+                    page,
+                    name,
                 },
-            )
+            })
 
             if (!data) throw new Error("No data")
 
--- app/pages/rankings/clans/[[...clansOptions]].tsx
+++ app/pages/rankings/clans/[[...clansOptions]].tsx
@@ -1,21 +1,42 @@
+import { AppLink } from "ui/base/AppLink"
+import { CLANS_PER_PAGE } from "@util/constants"
 import { RankingsLayout } from "@components/stats/rankings/RankingsLayout"
 import { SEO } from "@components/SEO"
+import { Spinner } from "ui/base/Spinner"
+import { cleanString } from "common/helpers/cleanString"
+import { cn } from "common/helpers/classnames"
+import { formatUnixTime } from "common/helpers/date"
+import { useClansRankings } from "@hooks/stats/useClansRankings"
+import { useDebouncedState } from "common/hooks/useDebouncedState"
+import { useEffect } from "react"
 import { useRouter } from "next/router"
 import type { NextPage } from "next"
 
 const ClansPage: NextPage = () => {
     const router = useRouter()
 
-    const { clansOptions } = router.query
+    const { clansOptions, clan = "" } = router.query
+
+    const [search, setSearch, immediateSearch] = useDebouncedState(
+        clan.toString(),
+        500,
+    )
 
     const [page = "1"] = Array.isArray(clansOptions) ? clansOptions : []
 
-    // const { clans, isLoading, isError } = useClansRankings(
-    //     page,
-    //     name.toString(),
-    // )
+    const { clans, isLoading, isError } = useClansRankings(page, search)
 
-    // if (isError || (!isLoading && !clans)) return <div>Error</div>
+    useEffect(() => {
+        window.history.replaceState(
+            "",
+            "",
+            `/rankings/clans/${page}?clan=${search}`,
+        )
+    }, [page, search])
+
+    if (isError || (!isLoading && !clans)) return <div>Error</div>
+
+    const showClanRank = !search
 
     return (
         <RankingsLayout
@@ -30,21 +51,22 @@ const ClansPage: NextPage = () => {
             currentBracket="clans"
             regions={[]}
             currentPage={page}
-            // hasPagination
+            hasPagination
+            hasSearch
+            search={immediateSearch}
+            setSearch={setSearch}
+            searchPlaceholder="Search clan..."
+            searchSubtitle="Search by clan name (exactly as it appears in-game)"
         >
             <SEO
                 title={`Brawlhalla Clans - Page ${page} • Corehalla`}
                 description={`Brawhalla Clans - Page ${page} • Corehalla`}
             />
-            Clan search will be implemented soon.
-            {/* <div className="py-4 w-full h-full flex items-center gap-4">
-                <p className="w-16 text-center">Rank</p>
-                <p className="w-16 text-center">Region</p>
+            <div className="p-4 w-full h-full flex items-center gap-4">
+                {showClanRank && <p className="w-16 text-center">Rank</p>}
                 <p className="flex-1">Name</p>
-                <p className="w-16 text-center">Games</p>
-                <p className="w-32 text-center">W/L</p>
-                <p className="w-20 text-center">Winrate</p>
-                <p className="w-40 pl-1">Elo</p>
+                <p className="w-40 pl-1 text-center">Created</p>
+                <p className="w-20 pl-1 text-center">XP</p>
             </div>
             {isLoading ? (
                 <div className="flex items-center justify-center h-48">
@@ -54,25 +76,32 @@ const ClansPage: NextPage = () => {
                 <div className="rounded-lg overflow-hidden border border-bg mb-4">
                     {clans.map((clan, index) => (
                         <div
-                            key={clan.clan_id}
+                            key={clan.id}
                             className={cn(
-                                "py-1 w-full h-full flex items-center gap-4 hover:bg-bg",
+                                "px-4 py-2 w-full h-full flex items-center gap-4 hover:bg-bg",
                                 { "bg-bgVar2": index % 2 === 0 },
                             )}
                         >
+                            {showClanRank && (
+                                <p className="w-16 h-full flex items-center justify-center text-xs">
+                                    {(parseInt(page, 10) - 1) * CLANS_PER_PAGE +
+                                        index +
+                                        1}
+                                </p>
+                            )}
                             <p className="flex flex-1 items-center">
-                                <AppLink href={`/stats/clans/${clan.clan_id}`}>
-                                    {cleanString(clan.clan_name)}
+                                <AppLink href={`/stats/clan/${clan.id}`}>
+                                    {cleanString(clan.name)}
                                 </AppLink>
                             </p>
-                            <p className="w-20 text-center">{clan.clan_xp}</p>
                             <div className="w-40 flex items-center justify-start">
-                                {clan.clan_create_date}
+                                {formatUnixTime(clan.created)}
                             </div>
+                            <p className="w-20 text-center">{clan.xp}</p>
                         </div>
                     ))}
                 </div>
-            )} */}
+            )}
         </RankingsLayout>
     )
 }
