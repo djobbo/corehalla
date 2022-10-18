607c4830f59dc93a6430f3d798a0541d932a83ce
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-10-18 18:19:34 +0200

    manager bot setup

--- discord-manager/src/index.tsx
+++ discord-manager/src/index.tsx
@@ -1,24 +1,60 @@
 import { GatewayIntentBits } from "discord.js"
-import { createClient } from "reaccord"
+import { createClient, createSlashCommand } from "reaccord"
 
 const {
     DISCORD_MANAGER_BOT_TOKEN = "",
     DISCORD_MANAGER_BOT_DEV_GUILD_ID,
     DISCORD_MANAGER_BOT_CLIENT_ID,
+    DISCORD_MANAGER_BOT_WELCOME_ROLES,
 } = process.env
 
 const client = createClient({
     token: DISCORD_MANAGER_BOT_TOKEN,
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+    ],
     devGuildId: DISCORD_MANAGER_BOT_DEV_GUILD_ID,
     clientId: DISCORD_MANAGER_BOT_CLIENT_ID,
 })
 
-client.listenTo("guildMemberAdd", (member) => {
-    member.guild.systemChannel?.send(`Welcome ${member} to the server!`)
+client.listenTo("guildMemberAdd", async (member) => {
+    member.guild.systemChannel?.send({
+        content: `👀 Welcome **${member}** to Corehalla!`,
+        allowedMentions: { users: [] },
+    })
+
+    if (DISCORD_MANAGER_BOT_WELCOME_ROLES) {
+        try {
+            await member.roles.add(
+                DISCORD_MANAGER_BOT_WELCOME_ROLES?.split(","),
+            )
+        } catch {
+            throw new Error(
+                `Failed to add roles to ${member.user.tag} (${member.id})`,
+            )
+        }
+    }
 })
 
-client.connect(() =>
-    // eslint-disable-next-line no-console
-    console.log(`🚀 Client connected as ${client.user?.username}!`),
-)
+client.listenTo("guildMemberRemove", async (member) => {
+    member.guild.systemChannel?.send({
+        content: `👋 Goodbye ${member}!`,
+        allowedMentions: { users: [] },
+    })
+})
+
+const infoCommand = createSlashCommand("info", "Get info about Corehalla") //
+    .exec(async (_, interaction) => {
+        await interaction.reply({
+            content: "Corehalla",
+        })
+    })
+
+client //
+    .registerCommand(infoCommand)
+    .connect(() =>
+        // eslint-disable-next-line no-console
+        console.log(`🚀 Client connected as ${client.user?.username}!`),
+    )
