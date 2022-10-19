c8d01b89cd127033c3496cb8dcdee76ea12ae30a
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-10-19 15:40:51 +0200

    add infobar

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { AppLink } from "ui/base/AppLink"
 import { Button } from "ui/base/Button"
 import { HiMenu } from "react-icons/hi"
+import { InfoBar } from "./InfoBar"
 import { SearchButton } from "../search/SearchButton"
 import { SiDiscord, SiGithub, SiTwitter } from "react-icons/si"
 import { cn } from "common/helpers/classnames"
@@ -22,96 +23,111 @@ export const Header = ({ className }: HeaderProps) => {
     const isLandingPage = router.pathname === "/"
 
     return (
-        <header className={cn({ "bg-bgVar2": !isLandingPage })}>
-            <div
-                className={cn(
-                    className,
-                    "flex items-center justify-between h-16 sm:h-20 px-4",
-                )}
-            >
-                <div className="flex items-center gap-4">
-                    <button
-                        type="button"
-                        className="block sm:hidden"
-                        onClick={() => {
-                            openSideNav()
-                        }}
-                    >
-                        <HiMenu size={24} />
-                    </button>
+        <>
+            <InfoBar>
+                <span>
+                    Brawlhalla maintenance ongoing. More info{" "}
                     <AppLink
-                        href="/"
-                        className="relative rounded-lg w-32 h-8 overflow-hidden"
+                        href="/discord"
+                        target="_blank"
+                        className="text-accentAlt font-semibold hover:text-text"
                     >
-                        <Image
-                            src="/images/logo.png"
-                            alt="Corehalla logo"
-                            layout="fill"
-                            objectFit="contain"
-                            objectPosition="center"
-                        />
-                    </AppLink>
-                </div>
-                <div className="flex items-center gap-2">
-                    <SearchButton
-                        bg={isLandingPage ? "bg-bgVar2" : "bg-bgVar1"}
-                        className="hidden sm:flex mr-2"
-                    />
-                    {isLoggedIn ? (
-                        <>
-                            {userProfile && (
-                                <>
-                                    <div className="relative rounded-lg w-8 h-8 overflow-hidden">
-                                        <Image
-                                            src={userProfile.avatarUrl}
-                                            alt={userProfile.username}
-                                            layout="fill"
-                                            objectFit="cover"
-                                            objectPosition="center"
-                                        />
-                                    </div>
-                                </>
-                            )}
-                            <Button onClick={signOut}>Sign out</Button>
-                        </>
-                    ) : (
-                        <Button onClick={signIn}>
-                            <SiDiscord size="16" className="mr-2" />
-                            Sign in
-                        </Button>
+                        here
+                    </AppLink>{" "}
+                    !
+                </span>
+            </InfoBar>
+            <header className={cn({ "bg-bgVar2": !isLandingPage })}>
+                <div
+                    className={cn(
+                        className,
+                        "flex items-center justify-between h-16 sm:h-20 px-4",
                     )}
-                    <div className="hidden md:flex items-center gap-1 ml-2">
-                        <AppLink
-                            className="text-textVar1 hover:text-text"
-                            href="/discord"
-                            target="_blank"
+                >
+                    <div className="flex items-center gap-4">
+                        <button
+                            type="button"
+                            className="block sm:hidden"
+                            onClick={() => {
+                                openSideNav()
+                            }}
                         >
-                            <SiDiscord size="16" className="mr-2" />
-                        </AppLink>
+                            <HiMenu size={24} />
+                        </button>
                         <AppLink
-                            className="text-textVar1 hover:text-text"
-                            href="/twitter"
-                            target="_blank"
+                            href="/"
+                            className="relative rounded-lg w-32 h-8 overflow-hidden"
                         >
-                            <SiTwitter size="16" className="mr-2" />
-                        </AppLink>
-                        <AppLink
-                            className="text-textVar1 hover:text-text"
-                            href="/github"
-                            target="_blank"
-                        >
-                            <SiGithub size="16" className="mr-2" />
+                            <Image
+                                src="/images/logo.png"
+                                alt="Corehalla logo"
+                                layout="fill"
+                                objectFit="contain"
+                                objectPosition="center"
+                            />
                         </AppLink>
                     </div>
+                    <div className="flex items-center gap-2">
+                        <SearchButton
+                            bg={isLandingPage ? "bg-bgVar2" : "bg-bgVar1"}
+                            className="hidden sm:flex mr-2"
+                        />
+                        {isLoggedIn ? (
+                            <>
+                                {userProfile && (
+                                    <>
+                                        <div className="relative rounded-lg w-8 h-8 overflow-hidden">
+                                            <Image
+                                                src={userProfile.avatarUrl}
+                                                alt={userProfile.username}
+                                                layout="fill"
+                                                objectFit="cover"
+                                                objectPosition="center"
+                                            />
+                                        </div>
+                                    </>
+                                )}
+                                <Button onClick={signOut}>Sign out</Button>
+                            </>
+                        ) : (
+                            <Button onClick={signIn}>
+                                <SiDiscord size="16" className="mr-2" />
+                                Sign in
+                            </Button>
+                        )}
+                        <div className="hidden md:flex items-center gap-1 ml-2">
+                            <AppLink
+                                className="text-textVar1 hover:text-text"
+                                href="/discord"
+                                target="_blank"
+                            >
+                                <SiDiscord size="16" className="mr-2" />
+                            </AppLink>
+                            <AppLink
+                                className="text-textVar1 hover:text-text"
+                                href="/twitter"
+                                target="_blank"
+                            >
+                                <SiTwitter size="16" className="mr-2" />
+                            </AppLink>
+                            <AppLink
+                                className="text-textVar1 hover:text-text"
+                                href="/github"
+                                target="_blank"
+                            >
+                                <SiGithub size="16" className="mr-2" />
+                            </AppLink>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="flex sm:hidden justify-center">
-                <SearchButton
-                    bg={isLandingPage ? "bg-bgVar2" : "bg-bgVar1"}
-                    className="w-full mx-4 mb-4"
-                    customWidth
-                />
-            </div>
-        </header>
+                <div className="flex sm:hidden justify-center">
+                    <SearchButton
+                        bg={isLandingPage ? "bg-bgVar2" : "bg-bgVar1"}
+                        className="w-full mx-4 mb-4"
+                        customWidth
+                    />
+                </div>
+            </header>
+        </>
     )
 }
