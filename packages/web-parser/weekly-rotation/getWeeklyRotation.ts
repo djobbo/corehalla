4595a48a4fa55234cb2c47e63bd4465bb5e4c68c
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-10-19 15:48:12 +0200

    fix weekly rotation getter

--- packages/web-parser/weekly-rotation/getWeeklyRotation.ts
+++ packages/web-parser/weekly-rotation/getWeeklyRotation.ts
@@ -4,7 +4,10 @@ import { parseBHArticlesPage } from "../bh-articles/parseBHArticlesPage"
 import axios from "axios"
 import type { Legend } from "bhapi/types"
 
-const PREFIX = "The free-to-play Legend rotation for this week includes: "
+const PREFIXES = [
+    "The free-to-play Legend rotation for this week includes: ",
+    "The free-to-play Legend rotation for this week features ",
+]
 
 export const getWeeklyRotation = async () => {
     const [article] = await parseBHArticlesPage(1, "weekly-rotation")
@@ -15,19 +18,23 @@ export const getWeeklyRotation = async () => {
 
     const $ = load(data)
 
-    const txt = $("i")
-        .filter((_, el) => $(el).text().startsWith(PREFIX))
-        .first()
-        .text()
+    for (const prefix of PREFIXES) {
+        const txt = $("i")
+            .filter((_, el) => $(el).text().startsWith(prefix))
+            .first()
+            .text()
 
-    const legendsNames = txt
-        .slice(PREFIX.length)
-        .replace(".", "")
-        .replace(/&|(and)/g, "")
-        .split(",")
-        .map((s) => s.trim())
+        if (!txt) continue
 
-    return legendsNames
-        .map((name) => legends.find((l) => l.bio_name === name))
-        .filter(Boolean) as Legend[]
+        const legendsNames = txt
+            .slice(prefix.length)
+            .replace(".", "")
+            .replace(/&|(and)/g, "")
+            .split(",")
+            .map((s) => s.trim())
+
+        return legendsNames
+            .map((name) => legends.find((l) => l.bio_name === name))
+            .filter(Boolean) as Legend[]
+    }
 }
