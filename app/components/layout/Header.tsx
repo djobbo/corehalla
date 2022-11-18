8c6b5c2fd13997553216b3337b2c005acde3d3ff
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-11-18 10:55:46 +0000

    auth issues resolved

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -24,7 +24,7 @@ export const Header = ({ className }: HeaderProps) => {
 
     return (
         <>
-            <AlertBar alert="AUTH_ISSUES" />
+            <AlertBar alert="AUTH_ISSUES_RESOLVED" />
             <header className={cn({ "bg-bgVar2": !isLandingPage })}>
                 <div
                     className={cn(
