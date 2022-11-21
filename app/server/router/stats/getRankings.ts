2ed9d08eaf56a1835de8d9a4490be63d9c8399df
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-11-21 09:50:48 +0000

    migrate rankings api to trpc

---
+++ app/server/router/stats/getRankings.ts
@@ -0,0 +1,62 @@
+import { getRankings } from "bhapi"
+import { getTeamPlayers } from "bhapi/helpers/getTeamPlayers"
+import { numericLiteralValidator } from "common/helpers/validators"
+import { publicProcedure } from "../../trpc"
+import { rankedRegionValidator } from "bhapi/constants"
+import { updateDBPlayerAliases } from "server/helpers/updateDBPlayerAliases"
+import { z } from "zod"
+
+export const get1v1Rankings = publicProcedure //
+    .input(
+        z.object({
+            region: rankedRegionValidator,
+            page: numericLiteralValidator,
+            name: z.optional(z.string()),
+        }),
+    )
+    .query(async (req) => {
+        const { region, page, name } = req.input
+        console.log("get1v1Rankings", req.input)
+
+        const rankings = await getRankings("1v1", region, page, name)
+
+        await updateDBPlayerAliases(
+            rankings.map((player) => ({
+                playerId: player.brawlhalla_id.toString(),
+                alias: player.name,
+            })),
+        ).catch((e) => {
+            console.error("Error updating player aliases", e)
+        })
+
+        return rankings
+    })
+
+export const get2v2Rankings = publicProcedure //
+    .input(
+        z.object({
+            region: rankedRegionValidator,
+            page: numericLiteralValidator,
+        }),
+    )
+    .query(async (req) => {
+        const { region, page } = req.input
+        console.log("get2v2Rankings", req.input)
+
+        const rankings = await getRankings("2v2", region, page)
+
+        await updateDBPlayerAliases(
+            rankings
+                .map(getTeamPlayers)
+                .flat()
+                .map((player) => ({
+                    playerId: player.id.toString(),
+                    alias: player.name,
+                }))
+                .flat(),
+        ).catch((e) => {
+            console.error("Error updating player aliases", e)
+        })
+
+        return rankings
+    })
--- app/util/constants.ts
+++ app/util/constants.ts
@@ -1,2 +1,4 @@
+const __DEV = process.env.NODE_ENV === "development"
+
 export const SEARCH_PLAYERS_ALIASES_PER_PAGE = 50
-export const CLANS_RANKINGS_PER_PAGE = 3
+export const CLANS_RANKINGS_PER_PAGE = __DEV ? 3 : 50
