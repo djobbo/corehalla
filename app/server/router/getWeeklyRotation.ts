b8412a5f4ff3fe05dda56c0ec60471cd26346e1e
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-12-10 03:06:45 +0000

    clean after emergency maintenance

--- app/server/router/getWeeklyRotation.ts
+++ app/server/router/getWeeklyRotation.ts
@@ -1,4 +1,4 @@
-// import { getWeeklyRotation as getWeeklyRotationFn } from "web-parser/weekly-rotation/getWeeklyRotation"
+import { getWeeklyRotation as getWeeklyRotationFn } from "web-parser/weekly-rotation/getWeeklyRotation"
 import { logInfo } from "logger"
 import { publicProcedure } from "@server/trpc"
 
@@ -6,7 +6,5 @@ export const getWeeklyRotation = publicProcedure //
     .query(async () => {
         logInfo("getWeeklyRotation")
 
-        // return getWeeklyRotationFn()
-
-        return []
+        return getWeeklyRotationFn()
     })
