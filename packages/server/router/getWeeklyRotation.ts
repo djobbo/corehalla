480f3912d68378460188c06409136e1d087dbb7a
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2023-01-03 20:26:34 +0000

    move server stuff to own package #90

--- packages/server/router/getWeeklyRotation.ts
+++ packages/server/router/getWeeklyRotation.ts
@@ -1,6 +1,6 @@
 import { getWeeklyRotation as getWeeklyRotationFn } from "web-parser/weekly-rotation/getWeeklyRotation"
 import { logInfo } from "logger"
-import { publicProcedure } from "@server/trpc"
+import { publicProcedure } from "../trpc"
 
 export const getWeeklyRotation = publicProcedure //
     .query(async () => {
