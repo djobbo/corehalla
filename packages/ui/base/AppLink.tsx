97b09007a4f952a838eebca394bc110be4ebcb9b
Djobbo-Victor <4998293+djobbo@users.noreply.github.com>
2023-10-11 04:46:45 +0200

    disable prefetching

--- packages/ui/base/AppLink.tsx
+++ packages/ui/base/AppLink.tsx
@@ -10,6 +10,7 @@ export const AppLink = ({ href, children, ...props }: AppLinkProps) => {
             href={href}
             {...props}
             rel={props.target === "_blank" ? "noreferrer" : undefined}
+            prefech={false}
         >
             {children}
         </Link>
