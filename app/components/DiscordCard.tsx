d904c96ad470c6fa5ff0fc86a6017883dedfa5fc
Djobbo <hi@djobbo.com>
2023-10-21 18:07:57 +0200

    3k members

--- app/components/DiscordCard.tsx
+++ app/components/DiscordCard.tsx
@@ -68,7 +68,7 @@ export const DiscordCard = () => {
                         containerClassName="w-20 h-20 rounded-3xl -mt-10 ml-6 border-8 border-bgVar2 overflow-hidden"
                     />
                     <span className="text-sm ml-2 mt-2 text-textVar1 text-center">
-                        2k+ discord members, and growing!
+                        3k+ discord members, and growing!
                     </span>
                 </div>
                 <div className="h-64 flex flex-col justify-between">
