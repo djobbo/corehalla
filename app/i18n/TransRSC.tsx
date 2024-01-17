053c3cc2a9a1078c757d469d8aac66d0a7afec49
Djobbo <hi@djobbo.com>
2024-01-17 01:12:54 +0000

    translations #96

--- app/i18n/TransRSC.tsx
+++ app/i18n/TransRSC.tsx
@@ -1,16 +1,9 @@
+"use client"
+
 import React from "react"
 
-import { TransNoContext, type TransProps } from "@lingui/react/server"
-import { getI18n } from "./i18n"
+import { Trans as BaseTrans, type TransProps } from "@lingui/react"
 
 export const Trans = (props: TransProps) => {
-    const i18n = getI18n()
-
-    if (!i18n) {
-        throw new Error(
-            "Lingui for RSC is not initialized. Use `setI18n()` first in root of your RSC tree.",
-        )
-    }
-
-    return <TransNoContext {...props} lingui={{ i18n }} />
+    return <BaseTrans {...props} />
 }
