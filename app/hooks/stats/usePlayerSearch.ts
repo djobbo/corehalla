8f67a695a70ea0854f788485e8cf9fafcd9b6cd1
Djobbo <hi@djobbo.com>
2024-10-04 20:10:45 +0000

    db destroyer

--- app/hooks/stats/usePlayerSearch.ts
+++ app/hooks/stats/usePlayerSearch.ts
@@ -1,17 +1,17 @@
 import { trpc } from "@util/trpc"
-import { useRankings1v1 } from "./useRankings"
 
 export const usePlayerSearch = (search: string) => {
-    const { rankings1v1, isLoading } = useRankings1v1("all", "1", search, {
-        enabled: !!search,
-    })
-    const { data: aliases } = trpc.searchPlayerAlias.useQuery(
+    // BMG DB Destroyer 🤦
+    // const { rankings1v1, isLoading } = useRankings1v1("all", "1", search, {
+    //     enabled: !!search,
+    // })
+    const { data: aliases, isLoading } = trpc.searchPlayerAlias.useQuery(
         { alias: search, page: "1" },
         { enabled: !!search },
     )
 
     return {
-        rankings1v1,
+        rankings1v1: [],
         isLoading,
         aliases: aliases ?? [],
     }
